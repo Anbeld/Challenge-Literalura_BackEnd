e07631cc103394bdb114fda33c7276c6072162e7
Anbeld <147835151+Anbeld@users.noreply.github.com>
2024-05-23 17:48:48 -0500

    Primer commit
    Proyecto Maven Challenge-Literalura_BackEnd con versión Java 22.

---
+++ src/main/java/com/aluracursos/Challenge_Literalura_BackEnd/ChallengeLiteraluraBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Challenge_Literalura_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraBackEndApplication.class, args);
+	}
+
+}
